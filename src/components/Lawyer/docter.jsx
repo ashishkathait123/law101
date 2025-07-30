1455edf7c690533071526d48b8cf49cbf7f6af60
ashish kathait <ashish2kathait2gmail.com>
2025-07-30 11:25:35 +0530

    small change in doctor.jsx

--- src/components/Lawyer/docter.jsx
+++ src/components/Lawyer/docter.jsx
@@ -118,7 +118,7 @@ const handleContactClick = (lawyer) => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 mt-12">
-      <h2 className="text-3xl font-bold text-center text-[#000080] mb-6">
+      <h2 className="text-3xl font-bold text-center text-[#000080] mb-6 mt-12">
         Our Lawyers
       </h2>
 
